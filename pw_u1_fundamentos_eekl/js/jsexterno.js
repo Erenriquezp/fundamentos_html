dfcd168fd6c4f93c42a46e8fab57e78d49bd66be
Edison Enriquez <erenriquezp@uce.edu.ec>
2025-11-05 19:46:53 -0500

    Taller 6 - Fundamentos JS

--- pw_u1_fundamentos_eekl/js/jsexterno.js
+++ pw_u1_fundamentos_eekl/js/jsexterno.js
@@ -57,3 +57,99 @@ function multiplicar(num1, num2) {
 function dividir(num1, num2) {
     return num1 / num2;
 }
+
+function fundamentosJS() {
+    /* Tipos de variables */
+    var nombre = "Ronaldo"; // antigua, considerada obsoleta
+    let apellido = "Enriquez" // variables cambiantes
+    let apellido2 = 15;
+    apellido2 = "Puetate";
+    let arreglo = [1, 2, 3, 4, 5, 6];
+    let diasSemana = ['Lunes', 'Martes', '...'];
+    const IVA = 12.8;
+    console.log('Fundamentos de JS');
+    console.log(nombre);
+    console.log(apellido2)
+    console.log(IVA);
+    console.log(arreglo);
+
+    //Arreglos
+    const arreglosDiasSemana = ['Lunes', 'Martes', 'Miercoles'];
+    arreglosDiasSemana.push('Jueves'); // Agregar al final
+    console.log(arreglosDiasSemana);
+    arreglosDiasSemana.unshift('Dias'); // Agregar al inicio
+    console.log(arreglosDiasSemana);
+
+    console.log(arreglosDiasSemana[0]);
+    console.log('Manejo de nulos, undefined y vacio');
+    arreglosDiasSemana.push(null);
+    arreglosDiasSemana.push('');
+    console.log(arreglosDiasSemana[5]);
+    console.log(arreglosDiasSemana[6]);
+    console.log(arreglosDiasSemana[7]);
+
+    const numerosPares = [2, 4, 6, 8];
+    const numerosImpares = [1, 3, 5, 7, 9];
+    const numerosTotales = numerosImpares.concat(numerosPares);
+    console.log(numerosTotales);
+
+    // Sentencias de Control
+
+    let edad = 19;
+    if(edad >= 18) {
+        console.log('Es mayor de Edad');
+    }else{
+        console.log('En menor de Edad');
+    }
+
+    let dia = 'lunes';
+    switch(dia){
+        case 'lunes':
+            console.log('Es lunes');
+            break;
+        case 'martes':
+            console.log('Es martes');
+            break;
+        default:
+            console.log('No es ninguno de esos dias');
+    }
+
+    for(let i=0; i<=5; i++){
+        console.log(i);
+    }
+
+    const frutas = ['manzana', 'sandia', 'papaya', 'naranja'];
+    for(let fruta of frutas) {
+        console.log(fruta);
+    }
+
+    // Manejo de Objetos
+    const profesor = {
+        nombre: 'Edison',
+        apellido: 'Cayambe',
+        edad: 36,
+        ecuatoriana: true,
+        genero: 'M',
+        ciudad: 'Quito'
+    };
+
+    console.log(profesor);
+    console.log(profesor.nombre);
+    profesor.apellido = 'Teran';
+    console.log(profesor);
+
+    if(profesor.ciudad === 'Quito') {
+        console.log('Es Quiteño');
+    }
+
+    if(profesor.edad !== 36) {
+        console.log('Es diferente de 36');
+    }else{
+        console.log('Es 36');
+    }
+
+    for(let clave in profesor) {
+        console.log(clave);
+        console.log(profesor[clave]);
+    }
+}
